cb0f9f2c3e9c426f2035eb0185d112812b6ac931
ADITYA <adityaverma2f@gmail.com>
2026-01-07 21:37:12 +0530

    Added BaseController

---
+++ src/main/java/com/sellify/platform/api/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.sellify.platform.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1")
+public abstract class BaseController {
+}
